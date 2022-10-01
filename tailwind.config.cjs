740856b71ee99107c3f56f8aed17023b0e5be84c
Pectics <pectics@outlook.com>
2022-10-01 23:32:16 +0800

    real fix

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -153,7 +153,6 @@ module.exports = {
       'gradient-to-bl': 'linear-gradient(to bottom left, var(--tw-gradient-stops))',
       'gradient-to-l': 'linear-gradient(to left, var(--tw-gradient-stops))',
       'gradient-to-tl': 'linear-gradient(to top left, var(--tw-gradient-stops))',
-      'peckotmc': "url('bg-peckotmc.png')",
     },
     backgroundOpacity: ({ theme }) => theme('opacity'),
     backgroundPosition: {
