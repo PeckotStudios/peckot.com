01692e123037f56afa0c45d80063f1fbb50799e7
Pectics <pectics@outlook.com>
2022-10-01 23:18:23 +0800

    folder changed

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ['./{src,public}/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   presets: [],
   darkMode: 'media', // or 'class'
   theme: {
